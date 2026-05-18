176c4e727bbd8766bf178f689d517ea85f26fa70
Sercan Celenk <sercancelenk@gmail.com>
2026-05-18 17:05:09 +0300

    feat: updated ui modes appearances

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
  * does (main.tsx checks LEEADMAN_PWA flag + presence of `serviceWorker` API).
  */
 
-const CACHE_VERSION = 'v9-pin-state';
+const CACHE_VERSION = 'v10-theme-polish';
 const CACHE_NAME = `leeadman-${CACHE_VERSION}`;
 
 const APP_SHELL = [
